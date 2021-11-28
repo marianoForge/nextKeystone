8b5c1406fe394520a3465027c1a6eb3a30a5c982
Mariano De Simone <desimone.mariano@gmail.com>
2021-11-28 17:02:20 -0300

    Backend

---
+++ backend/schema.ts
@@ -0,0 +1,106 @@
+/*
+Welcome to the schema! The schema is the heart of Keystone.
+
+Here we define our 'lists', which will then be used both for the GraphQL
+API definition, our database tables, and our Admin UI layout.
+
+Some quick definitions to help out:
+A list: A definition of a collection of fields with a name. For the starter
+  we have `User`, `Post`, and `Tag` lists.
+A field: The individual bits of data on your list, each with its own type.
+  you can see some of the lists in what we use below.
+
+*/
+
+// Like the `config` function we use in keystone.ts, we use functions
+// for putting in our config so we get useful errors. With typescript,
+// we get these even before code runs.
+import { list } from '@keystone-next/keystone';
+
+// We're using some common fields in the starter. Check out https://keystonejs.com/docs/apis/fields#fields-api
+// for the full list of fields.
+import {
+  text,
+  relationship,
+  password,
+  timestamp,
+  select,
+  image,
+} from '@keystone-next/keystone/fields';
+import { document } from '@keystone-next/fields-document';
+
+// We have a users list, a blogs list, and tags for blog posts, so they can be filtered.
+// Each property on the exported object will become the name of a list (a.k.a. the `listKey`),
+// with the value being the definition of the list, including the fields.
+export const lists = {
+  // Here we define the user list.
+  User: list({
+    // Here are the fields that `User` will have. We want an email and password so they can log in
+    // a name so we can refer to them, and a way to connect users to posts.
+    fields: {
+      name: text({ validation: { isRequired: true } }),
+      email: text({
+        validation: { isRequired: true },
+        isIndexed: 'unique',
+        isFilterable: true,
+      }),
+      // The password field takes care of hiding details and hashing values
+      password: password({ validation: { isRequired: true } }),
+      // Relationships allow us to reference other lists. In this case,
+      // we want a user to have many posts, and we are saying that the user
+      // should be referencable by the 'author' field of posts.
+      // Make sure you read the docs to understand how they work: https://keystonejs.com/docs/guides/relationships#understanding-relationships
+    },
+    // Here we can configure the Admin UI. We want to show a user's name and posts in the Admin UI
+    ui: {
+      listView: {
+        initialColumns: ['name'],
+      },
+    },
+  }),
+
+  Company: list({
+    fields: {
+      name: text({
+        validation: { isRequired: true },
+      }),
+      year: timestamp({
+        validation: { isRequired: true },
+      }),
+      phone: relationship({ ref: 'Phone.company', many: true }),
+    },
+  }),
+
+  Phone: list({
+    fields: {
+      name: text({
+        validation: { isRequired: true },
+      }),
+      company: relationship({ ref: 'Company.phone', many: false }),
+      document: document({
+        formatting: {
+          alignment: {
+            center: true,
+            end: true,
+          },
+          blockTypes: {
+            blockquote: true,
+          },
+          inlineMarks: {
+            bold: true,
+            italic: true,
+            underline: true,
+          },
+          headingLevels: [1, 2, 3],
+          listTypes: {
+            ordered: true,
+            unordered: true,
+          },
+        },
+        links: true,
+        layouts: [],
+        dividers: true,
+      }),
+    },
+  }),
+};
